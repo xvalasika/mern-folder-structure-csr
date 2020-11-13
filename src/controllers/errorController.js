3dbc382bb50ebff7334d0c5c22c6301a4b46fc8d
Hardpro <hardpro3000@gmail.com>
2020-11-13 20:20:24 +0100

    MERN folder structure initial commit

---
+++ src/controllers/errorController.js
@@ -0,0 +1,104 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = 'Invalid param!';
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const handleJwtError = () => {
+  return new AppError('Invalid token! Please log in again.', 401);
+};
+
+const handleJwtExpiredError = () => {
+  return new AppError('Your token has expired! Please log in again.', 401);
+};
+
+const sendErrorDev = (err, req, res) => {
+  // A) API
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(err.statusCode).json({
+      name: err.name,
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+
+  // B) RENDERED WEBSITE
+  console.log('ERROR ', err);
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong',
+    msg: err.message,
+  });
+};
+
+const sendErrorProd = (err, req, res) => {
+  // A) API
+  if (req.originalUrl.startsWith('/api')) {
+    // a) Operational, trusted error: send message to client
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+    // b) Programming or other unknown error: dont leak error details
+    // 1) Log error
+    console.log('ERROR ', err);
+    // 2) Send generic message
+    return res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+
+  // B) RENDERED WEBSITE
+  // a) Operational, trusted error: send message to client
+  if (err.isOperational) {
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong',
+      msg: err.message,
+    });
+  }
+  // b) Programming or other unknown error: dont leak error details
+  // 1) Log error
+  console.log('ERROR ', err);
+  // 2) Send generic message
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong',
+    msg: 'Please try again later.',
+  });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV.trim() === 'development') {
+    sendErrorDev(err, req, res);
+  } else if (process.env.NODE_ENV.trim() === 'production') {
+    let error = { ...err };
+    error.message = err.message;
+
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateFieldDB(err);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
+    if (err.name === 'JsonWebTokenError') error = handleJwtError();
+    if (err.name === 'TokenExpiredError') error = handleJwtExpiredError();
+
+    sendErrorProd(error, req, res);
+  }
+};
---
+++ src/server.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Handling UNCAUGHT EXCEPTION
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// CONNECT TO DATABASE
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// START THE SERVER
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
+
+// Handling UNHANDLED REJECTION
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED. Shutting down gracefully!');
+  server.close(() => {
+    console.log('Process terminated!');
+  });
+});
